d12195c342a9162186620c4b4e7d670ab556a11d
Dinesh kumar <Poladinesh2000@yahoo.com>
2017-04-11 23:20:41 -0400

    Adding Validations to calls..

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
-// var Cloudant = require('cloudant');
 // var request = require('request');
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
@@ -15,31 +14,48 @@ var password = 'admin123';
 
 // Initialize the library with my account.
 var cloudant = Cloudant({account:me, password:password});
-
+var db = cloudant.db.use("pizza_bot");
+var db1 = cloudant.db.use("user-db");
 // cloudant.db.list(function(err, allDbs) {
 //     console.log('All my databases: %s', allDbs.join(', '))
 // });
 
-var db = cloudant.db.use("pizza_bot");
-var db1 = cloudant.db.use("user-db");
 
-
-
+// Get List of products Available
 app.get('/product', function (req, res) {
     console.log('Got a get request.');
-    db.list(function(err, data) {
-        console.log(data);
-        // res.json(data);
-        res.send(data);
+    var data = {};
+    db.list(function(err, result) {
+        console.log("get list req");
+        if(result.length != 0){
+            data["error"] = 0;
+            data["products"] = result;
+            res.json(data);
+        }else{
+            data["error"] = 1;
+            data["products"] = "No Pizzas found..";
+            res.json(data);
+        }
     });
 
 });
 
+// Get details of the product Selected
 app.get('/product/:pizzaName',function(req,res) {
-    db.find({selector: {pizzaName: req.params.pizzaName}}, function (er, result) {
+    var data = {};
+    db.find({selector: {pizzaName: req.params.pizzaName}}, function (err, result) {
         console.log(result);
-        res.json(result);
-    })
+        // res.json(result);
+        if(result.length != 0){
+            data["error"] = 0;
+            data["products"] = result;
+            res.json(data);
+        }else{
+            data["error"] = 1;
+            data["products"] = "No Pizzas found..";
+            res.json(data);
+        }
+    });
 });
 
 // Insert User Details
@@ -48,23 +64,33 @@ app.post('/product', function (req, res) {
         "Quantity": req.body.quantity,
         "address": req.body.Address,
         "phone_number": req.body.phoneNumber
-    }
-//inserting data into cloudant...
+    };
+    var data ={};
 
-    db1.insert({vdetails}, vdetails.phone_number, function (err, body, header) {
-        if (err) {
-            return console.log('[db.insert] ', err.message);
-        }
-        res.json(result);
-        console.log('You have inserted the details.');
-        console.log(body);
-    });
+//inserting data into cloudant...
+    if(!!vdetails.Quantity && !!vdetails.address && !!vdetails.phone_number){
+        db1.insert({vdetails}, vdetails.phone_number, function (err, result) {
+            if (!!err) {
+                data["error"] = 1;
+                data["Products"] = result;
+            } else{
+                // res.json(result);
+                // console.log('You have inserted the details.');
+                // console.log(result);
+                data["error"] = 0;
+                data["Products"] = result;
+                console.log("Address Details inserted");
+            }
+            res.json(data);
+        });
+    }
+
 
 });
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
-})
+});
 
 
 
